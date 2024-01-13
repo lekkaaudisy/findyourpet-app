d1325d761e8dc96f27a2e8067d582fb791d4c803
Lekka Audisy <lekkaaudisy@gmail.com>
2024-01-14 02:37:12 +0700

    create register-page

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -2,7 +2,6 @@
     import { Button } from "$lib/components/ui/button";
     import google from "../../../images/google.png";
     import logo from "../../../images/letter-f.png";
-    import twitter from "../../../images/twitter.png";
 </script>
 
 <header>
@@ -15,20 +14,14 @@
 <main>
     <div class="flex justify-center items-center min-h-[80vh]">
         <div class="flex px-9 pb-12">
-            <div class="flex flex-col w-80 justify-center items-center">
+            <div class="flex flex-col w-auto justify-center items-center">
                 <div class="flex my-5 w-full items-center">
-                    <h1 class="text-3xl font-bold">Sign in to find<span class="text-[#009068]">yourpet</span></h1>
+                    <h1 class="text-3xl font-bold">Sign up to find<span class="text-[#009068]">yourpet</span></h1>
                 </div>
                 <div class="flex my-2 w-full items-center">
                     <Button variant="outline" class="w-full rounded-full">
                         <img src={google} alt="" class="size-5 mr-1">
-                        <p class="font-bold">Sign in with Google</p>
-                    </Button>
-                </div>
-                <div class="flex my-2 w-full items-center">
-                    <Button variant="outline" class="w-full rounded-full">
-                        <img src={twitter} alt="" class="size-5 mr-1">
-                        <p class="font-bold">Sign in with X</p>
+                        <p class="font-bold">Sign up with Google</p>
                     </Button>
                 </div>
                 <p class="text-lg">or</p>
@@ -46,20 +39,23 @@
                           <input type='password' class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900" placeholder=" " id="password"/>
                           <label for="password" class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">Password</label>
                         </div>
+                        <p class="flex items-center gap-1 mt-2 font-sans text-sm antialiased font-normal leading-normal text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 -mt-px">
+                              <path fill-rule="evenodd"
+                                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                                clip-rule="evenodd"></path>
+                            </svg>
+                            Use at least 8 characters, one uppercase, one lowercase and one number.
+                          </p>
                     </div>  
                 </div>
                 <div class="flex my-2 w-full items-center">
                     <Button class="w-full rounded-full bg-[#009068]">
-                        <p class="font-bold">Log in</p>
-                    </Button>
-                </div>
-                <div class="flex my-2 w-full items-center">
-                    <Button class="w-full rounded-full" variant="outline">
-                        <p class="font-bold">Forgot password?</p>
+                        <p class="font-bold">Create account</p>
                     </Button>
                 </div>
                 <div class="flex content-end mt-10">
-                    <p>Don't have an account?</p><a href="/register" class="ml-1 text-[#009068]">Sign Up</a>
+                    <p>Already have an account?</p><a href="/login" class="ml-1 text-[#009068]">Sign In</a>
                 </div>
             </div>
         </div>
